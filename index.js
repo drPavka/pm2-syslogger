a9d2efe89b993336d12ee506b83c1e2c7d5c6aee
Pavlo Dubenko <d.pavka@gmail.com>
2019-05-27 14:34:27 +0300

    module rolled back to commonjs

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-import { launchBus } from 'pm2';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var pm2_1 = require("pm2");
 var pmx = require('pmx');
 var posix = require('posix');
 var conf = pmx.initModule({});
-launchBus(function (err, bus) {
-    bus.on('log:err', function (data) {
+pm2_1.launchBus(function (err, bus) {
+    bus.on('log:out', function (data) {
         posix.openlog(data.process.name, { ndelay: true, pid: true }, conf.priority);
         posix.syslog(conf.facility, data.data);
         posix.closelog();
     });
-    bus.on('log:out', function (data) {
-        posix.openlog(data.process.name, { ndelay: true, pid: true }, 'user');
-        posix.syslog('info', data.data);
+    bus.on('log:err', function (data) {
+        posix.openlog(data.process.name, { ndelay: true, pid: true }, conf['error-priority']);
+        posix.syslog(conf['error-facility'], data.data);
         posix.closelog();
     });
 });
