f933727cf082517fbd0d21b8fa621a2a24cc213f
Pavlo Dubenko <d.pavka@gmail.com>
2019-05-27 14:28:31 +0300

    changed

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,15 @@ const posix = require('posix');
 const conf = pmx.initModule({});
 
 launchBus((err: Error, bus: any) => {
-    bus.on('log:err', (data: any) => {
+    bus.on('log:out', (data: any) => {
         posix.openlog(data.process.name, {ndelay: true, pid: true}, conf.priority);
         posix.syslog(conf.facility, data.data);
         posix.closelog();
     });
 
-    bus.on('log:out', (data: any) => {
-        posix.openlog(data.process.name, {ndelay: true, pid: true}, 'user');
-        posix.syslog('info', data.data);
+    bus.on('log:err', (data: any) => {
+        posix.openlog(data.process.name, {ndelay: true, pid: true}, conf['error-priority']);
+        posix.syslog(conf['error-facility'], data.data);
         posix.closelog();
     })
 });
