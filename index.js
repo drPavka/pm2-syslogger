e488b0c236dea9cb2377d31c012acc6a91c52cc3
Pavlo Dubenko <d.pavka@gmail.com>
2019-09-06 19:22:29 +0300

    fixed wrong order of params

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var posix = require('posix');
 var conf = pmx.initModule({});
 pm2_1.launchBus(function (err, bus) {
     bus.on('log:out', function (data) {
-        posix.openlog(data.process.name, { ndelay: true, pid: true }, conf.priority);
-        posix.syslog(conf.facility, data.data);
+        posix.openlog(data.process.name, {ndelay: true, pid: true}, conf.facility);
+        posix.syslog(conf.priority, data.data);
         posix.closelog();
     });
     bus.on('log:err', function (data) {
-        posix.openlog(data.process.name, { ndelay: true, pid: true }, conf['error-priority']);
-        posix.syslog(conf['error-facility'], data.data);
+        posix.openlog(data.process.name, {ndelay: true, pid: true}, conf['error-facility']);
+        posix.syslog(conf['error-priority'], data.data);
         posix.closelog();
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@ const conf = pmx.initModule({});
 
 launchBus((err: Error, bus: any) => {
     bus.on('log:out', (data: any) => {
-        posix.openlog(data.process.name, {ndelay: true, pid: true}, conf.priority);
-        posix.syslog(conf.facility, data.data);
+        posix.openlog(data.process.name, {ndelay: true, pid: true}, conf.facility);
+        posix.syslog(conf.priority, data.data);
         posix.closelog();
     });
 
     bus.on('log:err', (data: any) => {
-        posix.openlog(data.process.name, {ndelay: true, pid: true}, conf['error-priority']);
-        posix.syslog(conf['error-facility'], data.data);
+        posix.openlog(data.process.name, {ndelay: true, pid: true}, conf['error-facility']);
+        posix.syslog(conf['error-priority'], data.data);
         posix.closelog();
     })
 });
